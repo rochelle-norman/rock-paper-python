27dc8f89cd902353116933b74c1f2d6362c3d816
Rochelle Norman <36737111+rochelle-norman@users.noreply.github.com>
2021-03-08 20:23:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,38 @@
-#
-#
-# from random import randint
-#
-# #create a list of play options
-# t = ["Rock", "Paper", "Scissors"]
-#
-# #assign a random play to the computer
-# computer = t[randint(0,2)]
-#
-# #set player to False
-# player = False
-#
-# while player == False:
-# #set player to True
-#     player = input("Rock, Paper, Scissors?")
-#     if player == computer:
-#         print("Tie!")
-#     elif player == "Rock":
-#         if computer == "Paper":
-#             print("You lose!", computer, "covers", player)
-#         else:
-#             print("You win!", player, "smashes", computer)
-#     elif player == "Paper":
-#         if computer == "Scissors":
-#             print("You lose!", computer, "cut", player)
-#         else:
-#             print("You win!", player, "covers", computer)
-#     elif player == "Scissors":
-#         if computer == "Rock":
-#             print("You lose...", computer, "smashes", player)
-#         else:
-#             print("You win!", player, "cut", computer)
-#     else:
-#         print("That's not a valid play. Check your spelling!")
-#     #player was set to True, but we want it to be False so the loop continues
-#     player = False
-#     computer = t[randint(0,2)]
 
-import random
 
-user_action = input("Enter a choice (rock, paper, scissors): ")
-possible_actions = ["rock", "paper", "scissors"]
-computer_action = random.choice(possible_actions)
+from random import randint
 
-while True:
-#f string
-    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
-    if user_action == computer_action:
-        print(f"Both players selected {user_action}. It's a tie!")
-    elif user_action == "rock":
-        if computer_action == "scissors":
-            print("Rock smashes scissors! You win!")
+#create a list of play options
+t = ["Rock", "Paper", "Scissors"]
+
+#assign a random play to the computer using randint
+computer = t[randint(0,2)]
+
+#set player to False
+player = False
+
+while player == False:
+#set player to True
+    player = input("Rock, Paper, Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
         else:
-            print("Paper covers rock! You lose.")
-    elif user_action == "paper":
-        if computer_action == "rock":
-            print("Paper covers rock! You win!")
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
         else:
-            print("Scissors cuts paper! You lose.")
-    elif user_action == "scissors":
-        if computer_action == "paper":
-            print("Scissors cuts paper! You win!")
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
         else:
-            print("Rock smashes scissors! You lose.")
-
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != "y":
-        break #breaks the loop if they choose no
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid. Check your spelling and Don't leave a space")
+    #player was set to True, but we want it to be False so the loop continues or end after one game nad leave this out
+    # player = False
+    # computer = t[randint(0,2)]
